bde4c0548ef4bb1ef248abe7b961945003bf393d
Boluwatife Afariogun <afariogunbolu@gmail.com>
2025-12-01 18:52:37 -0500

    Initial backend and frontend structure, models and controllers

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE || 'http://localhost:5000/api',
-    headers: { 'Content-Type': 'application/json' }
+    baseURL: 'http://localhost:5000/api',
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token){
+        config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
